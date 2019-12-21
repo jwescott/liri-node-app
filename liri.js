6874d1190ea8df1f15986516bbb69990fe503fe2
jwescott <jwescott@mail.usf.edu>
2019-12-20 22:40:36 -0500

    got the omdb search to work

--- liri.js
+++ liri.js
@@ -1,5 +1,9 @@
+var axios = require("axios");
+
+
 require("dotenv").config();
 var keys = require("./keys.js");
+
 
 
 
@@ -56,8 +60,60 @@ function findSpotify() {
 
 }
 
+
+// * Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Rotten Tomatoes Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+
+
 function findMovie() {
-  
+  // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
+
+
+// Then run a request with axios to the OMDB API with the movie specified
+axios.get("http://www.omdbapi.com/?t="+mediaSearch+"&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    console.log("Title: ",response.data.Title);
+    console.log("Year: ",response.data.Year);
+    console.log("IMDB Rating: ",response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating: ",response.data.Ratings[1].Value);
+    console.log("Country: ",response.data.Country);
+    console.log("Language: ",response.data.Language);
+    console.log("Plot: ",response.data.Plot);
+    console.log("Actors: ",response.data.Actors);
+    
+
+
+    
+
+    
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+
 }
 
 // This will show the following information about the song in your terminal/bash window
@@ -71,10 +127,7 @@ function findMovie() {
 // The album that the song is from
 
 
-
-function findMovie() {
-
-}
+ 
 
 function findLiri() {
 
