d6db1492487c5422d3727082b2842c26ad60c83d
jwescott <jwescott@mail.usf.edu>
2019-12-20 23:05:04 -0500

    got findConcert function to work as well

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var axios = require("axios");
 require("dotenv").config();
 var keys = require("./keys.js");
 
-
+var moment = require("moment")
 
 
 var Spotify = require("node-spotify-api");
@@ -33,6 +33,51 @@ switch (command) {
 }
 
 function findConcert() {
+
+
+  axios.get("https://rest.bandsintown.com/artists/" + mediaSearch + "/events?app_id=codingbootcamp").then(
+  function(response) {
+   
+    console.log(response.data);
+    var artistData = response.data
+
+    for (let i = 0; i < artistData.length; i++) {
+      console.log("Name of Venue: ",artistData[i].venue.name);
+      console.log("Venue Location: ",artistData[i].venue.city,artistData[i].venue.region,artistData[i].venue.country);
+      console.log("Date: ",moment(artistData[i].datetime,"YYYY-MM-DD").format("MM/DD/YYYY"));
+      
+      
+      
+    }
+    
+
+
+    
+
+    
+  })
+  .catch(function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  });
+
+  // "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
 }
 
